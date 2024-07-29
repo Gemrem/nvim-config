663afdca35d511717649102f2d2ce674573a0a05
Lucas Huebner <huebner@inter-medien.com>
2024-07-29 22:35:06 +0200

    Replaced lsp-zero with mason-lspconfig

--- lua/gemrem/lazy/lsp.lua
+++ lua/gemrem/lazy/lsp.lua
@@ -1,37 +1,21 @@
 return {
     {
-        'VonHeikemen/lsp-zero.nvim', 
+        "williamboman/mason.nvim",
         dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim"
+            "williamboman/mason-lspconfig.nvim",
+            "neovim/nvim-lspconfig",
         },
-        branch = 'v3.x',
+
         config = function()
-            local lsp_zero = require('lsp-zero')
-
-            lsp_zero.on_attach(function(client, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                lsp_zero.default_keymaps({buffer = bufnr})
-            end)
-
-            require('mason').setup({})
-            require('mason-lspconfig').setup({
-                -- Replace the language servers listed here 
-                -- with the ones you want to install
-                ensure_installed = {'tsserver', 'rust_analyzer'},
-                handlers = {
-                    function(server_name)
-                        require('lspconfig')[server_name].setup({})
-                    end,
-                },
-            })
-
+            require("mason").setup()
+            require("mason-lspconfig").setup {
+                ensure_installed = { "lua_ls", "rust_analyzer" },
+            }
+            require("mason-lspconfig").setup_handlers {
+                function (server_name)
+                    require("lspconfig")[server_name].setup {}
+                end,
+            }
         end
-
-     },
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
+    }
 }
