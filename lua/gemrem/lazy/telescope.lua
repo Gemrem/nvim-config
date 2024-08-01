b9cfd0af0c161e23f7aba2a4415214a12501c281
Lucas Huebner <huebner@inter-medien.com>
2024-08-01 22:59:00 +0200

    Split file and git key binds

--- lua/gemrem/lazy/telescope.lua
+++ lua/gemrem/lazy/telescope.lua
@@ -1,15 +1,19 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-     dependencies = { 'nvim-lua/plenary.nvim' },
-     config = function()
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-            vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-            vim.keymap.set('n', '<leader>fc', builtin.colorscheme, {})
-     end
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fc', builtin.colorscheme, {})
+
+        -- Git mappings
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>gh', builtin.git_commits, {})
+        vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+
+end
 }
 
