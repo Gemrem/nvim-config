402b5d68b4b002afcd83373fc643538bc4a9df76
Lucas Huebner <huebner@inter-medien.com>
2024-08-01 22:50:58 +0200

    Add keybinds for fugitive

--- lua/gemrem/lazy/fugitive.lua
+++ lua/gemrem/lazy/fugitive.lua
@@ -1,4 +1,11 @@
 return {
     "tpope/vim-fugitive",
-
+        config = function()
+            vim.keymap.set("n", "<leader>gs", "<cmd>Git<CR>")
+            vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>")
+            vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>")
+            vim.keymap.set("n", "<leader>gd", "<cmd>Gdiff<CR>")
+            vim.keymap.set("n", "<leader>gl", "<cmd>Git log<CR>")
+            vim.keymap.set("n", "<leader>gw", "<cmd>Gwrite<CR>")
+    end,
 }
